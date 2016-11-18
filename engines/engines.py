14a5a79acc937cbd0acac93ec5e172f0a2aa6d30
Tim Dobbins <tmthyjames@gmail.com>
2016-11-17 22:01:07 -0600

    update gitignore

---
+++ engines/engines.py
@@ -0,0 +1,8 @@
+# default connection string info here
+driver = 'postgresql'
+username = 'username'
+password = 'password'
+host = 'localhost'
+port = '5432'
+# empty until set by user
+default_db = ''
