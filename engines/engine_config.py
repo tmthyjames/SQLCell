9f3157fe7ed80619bd80367122827a2c9bc7aad2
Xiaochuan Yu <xcyu.se@gmail.com>
2017-02-13 17:50:33 -0500

    Update engine_config.py

--- engines/engine_config.py
+++ engines/engine_config.py
@@ -2,8 +2,8 @@ import os
 
 # default connection string info here
 driver = 'postgresql'
-username = os.getenv('DB_USERNAME')
-password = os.getenv('DB_PASSWORD')
+username = os.getenv('DB_USERNAME') or 'undefined'
+password = os.getenv('DB_PASSWORD') or 'undefined'
 host = 'localhost'
 port = '5432'
 # empty until set by user
