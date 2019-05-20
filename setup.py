d21c6e878c2482a45f2eb2e0c925791d9176370c
Dobbins, Timothy <tmthyjames@gmail.com>
2019-05-20 06:15:51 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sqlcell",
-    version="0.2.0.14",
+    version="0.2.0.15",
     description="run sql in jupyter notebooks or jupyter lab",
     license="MIT",
     author="Tim Dobbins",
