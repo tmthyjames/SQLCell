6223249b8b2a94312f0897fdae54135a74aafdbd
Dobbins, Timothy <tmthyjames@gmail.com>
2019-10-02 16:02:15 -0500

    make hooks server agnostic

--- sqlcell/hooks.py
+++ sqlcell/hooks.py
@@ -25,30 +25,31 @@ class HookHandler(EngineHandler):
         for hook in hooks:
             hook = hook.strip()
             if hook:
-                key_engine, cmd = [i.strip() for i in hook.split('=', 1)]
-                key, engine = key_engine.split(' ')
-                engine = engine
-                cmds_to_add.append((key, engine, cmd))
+                key, cmd = [i.strip() for i in hook.split('=', 1)]
+                # key, engine = key_engine.split(' ')
+                # engine = engine
+                cmds_to_add.append((key, cmd))
 
-        for key, engine, cmd in cmds_to_add:
-            engine = self.db_info.get(engine, engine)
-            is_engine = self.is_engine(engine)
-            if not is_engine:
-                raise Exception('Alias not found or engine argument error')
-            self.session.add(self.Hooks(key=key, engine=engine, cmd=cmd))
+        for key, cmd in cmds_to_add:
+            # engine = self.db_info.get(engine, engine)
+            # is_engine = self.is_engine(engine)
+            # if not is_engine:
+                # raise Exception('Alias not found or engine argument error')
+            self.session.add(self.Hooks(key=key, engine='', cmd=cmd))
         self.session.commit()
         return self
         
     def run(self, cell, engine_var):
         cell = cell.replace('~', '').split(' ')
-        cell, cmd_args = cell[0], cell[1:]
-        hook_query = self.session.query(self.Hooks).filter_by(key=cell).first()
+        engine_alias, sql, cmd_args = cell[0], cell[1], cell[2:]
+        hook_query = self.session.query(self.Hooks).filter_by(key=sql).first()
         hook_cmd = hook_query.cmd
-        if engine_var:
-            engine = self.get_engine(engine_var)
-            hook_engine = create_engine(str(engine.url))
-        else:
-            hook_engine = create_engine(hook_query.engine)
+        hook_engine = self.get_engine(engine_alias)
+        # if engine_var:
+        #     engine = self.get_engine(engine_var)
+        #     hook_engine = create_engine(str(engine.url))
+        # else:
+        #     hook_engine = create_engine(hook_query.engine)
         self.hook_engine = hook_engine
         return hook_engine, hook_cmd.format(*cmd_args)
 
